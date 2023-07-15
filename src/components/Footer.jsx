59c1c6b9003a3f0075641599eee8b547b63dec7c
Aurora <eunbipower@gmail.com>
2023-07-16 02:44:37 +0900

    style: Navbar 로고 및 로그인 버튼

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 const StFooter = styled.div`
     height: 100px;
-    background-color: #8DABB5;
+    background-color: #41613c;
 `;
 
 export default function Footer() {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,97 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
+// import { BsFillPersonFill } from "react-icons/bs";
+import logo from "../image/logo.png";
 
 const Nav = styled.div`
     height: 100px;
-    background-color: #8dabb5;
+    background-color: #41613c;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 0 8rem;
+
+    img {
+        width: 100px;
+        height: 100px;
+    }
+    div {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin: 10px;
+        button:first-child {
+            width: 100px;
+            height: 50px;
+            color: #fff;
+            background-color: #41613c;
+            font-size: 15px;
+            font-weight: bold;
+            border: none;
+            border-radius: 8px;
+            cursor: pointer;
+            /* &:hover {
+                background-color: #fff;
+                color: #41613c;
+            } */
+        }
+        button:nth-child(2) {
+            width: 100px;
+            height: 50px;
+            color: #41613c;
+            font-size: 15px;
+            font-weight: bold;
+            border: none;
+            border-radius: 8px;
+            cursor: pointer;
+        }
+
+        /* button:nth-child(2):hover {
+            background-color: #41613c;
+            color: #fff;
+        } */
+        .human {
+            width: 50px;
+            height: 50px;
+            color: #fff;
+            margin: 10px;
+            cursor: pointer;
+        }
+    }
 `;
 
 export default function Navbar() {
+    const navigate = useNavigate();
     return (
         <>
-            <Nav></Nav>
+            <Nav>
+                <Link to="/">
+                    <img
+                        src={logo}
+                        alt="요기어때 로고"
+                    />
+                </Link>
+                <div>
+                    <button
+                        onClick={() => {
+                            navigate("/login");
+                        }}
+                    >
+                        LOG IN
+                    </button>
+                    <button
+                        onClick={() => {
+                            navigate("/signup");
+                        }}
+                    >
+                        SIGN UP
+                    </button>
+                    {/* <BsFillPersonFill className="human" /> */}
+                </div>
+            </Nav>
         </>
     );
 }
